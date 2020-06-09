61b3b8e5ed17d92eb8ed3f2994269cc67eca9f60
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-06-09 07:51:26 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,11 @@
 def display_board(board)
   line = "|"
-  row = "-----------"
   
-  puts "#{board[0]} #{line} #{board[1]} #{line} #{board[2]}"
-  puts "#{row}"
-  puts "#{board[3]} #{line} #{board[4]} #{line} #{board[5]}"
-  puts "#{row}"
-  puts "#{board[6]} #{line} #{board[7} #{line} #{board[8]}"
+  puts "#{board[0]} | #{board[1]} | #{board[2]}"
+  puts "-----------"
+  puts "#{board[3]} | #{board[4]} | #{board[5]}"
+  puts "-----------"
+  puts "#{board[6]} | #{board[7} | #{board[8]}"
 end
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
