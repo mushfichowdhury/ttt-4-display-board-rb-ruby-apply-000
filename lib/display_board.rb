c7b86855f872c5694aa574d80557f9f6bd34987b
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-06-09 07:21:32 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,15 @@
+def display_board
+  puts "' ', '|', ' ', '|', ' '"
+  puts "' ', '|', ' ', '|', ' '"
+  puts "' ', '|', ' ', '|', ' '"
+end
+
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+
+
+
+
 # Define display_board that accepts a board and prints
 # out the current state.
