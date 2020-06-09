ce89bb40ba4425ebe962e52be65dc834d1613b17
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-06-09 07:37:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -15,9 +15,10 @@ def display_board(board)
   
   puts "#{board[0]} #{line} #{board[1]} #{line} #{board[2]}"
   puts "#{row}"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+  puts "#{board[3]} #{line} #{board[4]} #{line} #{board[5]}"
+  puts "#{row}"
+  puts "#{board[6]} #{line} #{board[7} #{line} #{board[8]}"
+  puts "#{row}"
 end
 
 
