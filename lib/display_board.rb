36f2569ea316e07cd98af38bccae047056e1ea44
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-06-09 07:47:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -11,8 +11,33 @@ def display_board(board)
   puts "#{board[6]} #{line} #{board[7} #{line} #{board[8]}"
 end
 
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
+board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+board = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
+board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
+board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]  
+
+   
+     
+
+    
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq(" X | O | O ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq(" X | O | O ")
 
 
+      
 
-# Define display_board that accepts a board and prints
-# out the current state.
+      output = capture_puts{ display_board(board) }
+      rows = output.split("\n")
+
+      expect(rows[0]).to eq(" X | O | X ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq(" O | X | X ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq(" O | X | O ")
+    end
